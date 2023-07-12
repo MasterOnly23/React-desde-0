84ef436bbaa9833df8940dbe62d08f127eeec8d8
jfdaza <mainekko23@gmail.com>
2023-07-12 17:33:11 -0300

    react de 0

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+
+export function App () {
+    return(
+        <article>
+            <header>
+                <img src="https://unavatar.io/masteronly23" alt="avatar masteronly23" />
+                <div>
+                    <strong>Juan Felipe Daza</strong>
+                    <span>@masteronly23</span>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
